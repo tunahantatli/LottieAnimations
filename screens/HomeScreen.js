2872527a3fb455af8dc0299c2e2a0eb032ab23cb
tunahantatli <tunahantatl@gmail.com>
2024-02-28 21:22:08 +0300

    final

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,25 @@
+import { useNavigation } from '@react-navigation/native'
+import LottieView from 'lottie-react-native'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+  const rstButton =()=>{
+    navigation.navigate('OnBoarding')
+  }
   return (
-    <View>
-        <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center">
+      <View>
+        <LottieView style={{width:350, height:300}} source={require('../assets/animations/confetti.json')} autoPlay loop={false} />
+      </View>
+      <Text className="mt-3 mb-3 text-[21px]">Wellcome to App</Text>
+      <TouchableOpacity onPress={rstButton} className="bg-black p-3 rounded-full w-20 justify-center items-center">
+        <Text className="text-white text-[17px]">Reset</Text> 
+      </TouchableOpacity>
+    </SafeAreaView>
+  
   )
 }
 
